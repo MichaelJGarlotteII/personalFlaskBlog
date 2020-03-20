67ee3ba96ceb0d68598df5879e8c5c00ab010cc0
James Graham <james.graham@toyotaconnected.com>
2020-03-19 20:23:50 -0500

    upvotes work now

--- flaskBlog/static/frontend.js
+++ flaskBlog/static/frontend.js
@@ -1,12 +1,12 @@
 function upvote_post(){
     console.log('upvoting post')
     $.post("http://localhost:5000/api/post/"+post_id+"/upvote", function(data){
-        alert('post upvoted!')
+        alert('post upvoted!');
         console.log('getting upvotes')
         $.get("http://localhost:5000/api/post/"+post_id+"/upvote", function(data) {
-            
-            $("#post-upvotes").innerHTML = data.upvotes
-        })
+            console.log(data.upvotes)
+            $("#post-upvotes").text(data.upvotes)
+        });
         
     })
 }
